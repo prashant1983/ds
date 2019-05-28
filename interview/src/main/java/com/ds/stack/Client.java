3883a74c28472c14ac851cafba6f44f94f78d3dd
prashant1983 <gsprashant@gmail.com>
2019-05-28 11:15:04 +0530

    queue adde

--- interview/src/main/java/com/ds/stack/Client.java
+++ interview/src/main/java/com/ds/stack/Client.java
@@ -11,9 +11,10 @@ public class Client {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		StackImpl<String> stack = new StackImpl<String>();
+	public static void main(String[] args) throws Exception {
+		StackIF<String> stack = new StackImpl<String>();
 		stack.push("Prashant");
 		stack.push("Sushant");
 		stack.push("Sushmita");
